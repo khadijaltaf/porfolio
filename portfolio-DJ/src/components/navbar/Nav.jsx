e60af62ada95d71e09f600258973fc545edb196b
Khadija <khadijaltaf08@gmail.com>
2023-07-27 21:07:39 +0500

    started navbar

--- portfolio-DJ/src/components/navbar/Nav.jsx
+++ portfolio-DJ/src/components/navbar/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {BiHome} from 'react-icons/bi'
+import {AiOutlineUser} from 'react-icons/ai'
+import {AiOutlineBook} from 'react-icons/ai'
+import {RiServiceLine} from 'react-icons/ri'
+import {IoMdContact} from 'react-icons/io'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <a href="#" ><BiHome /></a>
+      <a href="#about" ><AiOutlineUser /></a>
+      <a href="#experience" ><AiOutlineBook /></a>
+      <a href="#serivesProvider" ><RiServiceLine /></a>
+      <a href="#contact" ><IoMdContact /></a>
+    </div>
   )
 }
 
